4dbf8b9a6f51caf85b8fc6681e773b74fda550e3
Lane17027 <lanenichols17027@gmail.com>
2024-09-24 23:04:24 -0600

    setting up the Chat functionality

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
 export default function Chat() {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {}, []);
   return (
-    <div>Chat</div>
-  )
+    <Container>
+      <div className="container">Hi</div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
